32de755af1bb8efb816bb744a4dd8ee3700b345c
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-04-24 18:45:52 +0530

    Add files via upload

---
+++ sanitize.py
@@ -0,0 +1,30 @@
+from future import print_function
+from future import division
+import pandas as pd
+import ServerAPI as server_api
+import sys
+sys.path.append('../')
+
+class Sanitizer():
+    def __init__(self,bnk_url):
+        self.ApplicationData = server_api.fetch_from(bnk_url)
+    def performSanitization():
+        df = getApplication()
+        SanitizedApplicationData = df[['reports','expenditure','active','income']]
+        return SanitizedApplicationData
+
+    def getApplication():
+        return pd.read_csv(ApplicationData)
+
+
+def getSanitizedApplicationData(obj):
+    SanitizedApplicationData=obj.performSanitization()
+    return SanitizedApplicationData
+
+
+if  __init__ == "__main__":
+
+    callsan = Sanitizer(bnk_url)
+    df=getSanitizedApplicationData(callsan)
+    df.to_csv('SanitizedApplication.csv')
+
