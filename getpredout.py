eb6d78ddccced23e0c35bf562e201352ca9bb46c
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-05 02:07:46 +0530

    Update getpredout.py

--- getpredout.py
+++ getpredout.py
@@ -5,14 +5,21 @@ from future import division
 import sys
 sys.path.append('../')
 
-### data is a list of lists like data=[['tom', 10], ['nick', 15], ['juli', 14]]
+def check(element):
+	if element == '1':
+		return 'Yes'
+	else:
+		 return 'No'
 
-
-df = pd.DataFrame(data, columns = ['ApplicationId', 'Status']) 
-df.to_csv('prediction.csv')
-
-
-session = ftplib.FTP('server.address.com','USERNAME','PASSWORD')
+def setApprovalPrediction(df):
+  df['Status'] = df['Status'].apply(check)
+  df = df.drop(feat_cols,axis=1)
+  df.to_csv('prediction.csv', index=False)
+  
+feat_cols = ['reports','expenditure','age','income']
+df = pd.read_csv('unapproved_prediction.csv')
+setApprovalPrediction(df)
+session = ftplib.FTP('server.address.com','USERNAME','PASSWORD')  #awaiting ftp server help from team
 file = open('prediction.csv','rb')                  # file to send
 session.storbinary('STOR prediction.csv', file)     # send the file
 file.close()                                    # close file and FTP
