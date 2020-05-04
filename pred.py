3928590e0d9c9840ad718bbddc9e7317bc9c88a4
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-05 02:00:18 +0530

    Update pred.py

--- pred.py
+++ pred.py
@@ -5,6 +5,7 @@ import joblib
 import requests
 import sklearn
 import sys
+import sklearn.util.random as rnd
 sys.path.append('../')
 
 class Extractor():
@@ -14,26 +15,33 @@ class Extractor():
         return df[self.features]
         
 class Predictor():
-    def __init__(self,path,model_path):
+    def __init__(self,path,modelfile):
         self.datapath = path
-        self.model_path = model_path
+        self.modelfile = modelfile
     def getSanitizedApplicationData():
         return pd.read_csv(self.datapath)
     def callExtractor(settings):
         return Extractor(settings)
     def model_load():
-        return joblib.load(self.model_path)
+        return joblib.load(self.modelfile)
     def prediction_function(df,model):
-        return model.predict(df)
+        card = np.asarray(len(df))
+        card = model.predict(df)
+        return card
+
 
 if __init__ == '__main__':
     url = 'https://raw.githubusercontent.com/ucalyptus/scikit-on-gRPC/master/model.joblib'
-    r = requests.get(url, allow_redirects=True)
-    open('model.joblib', 'wb').write(r.content)
+    r = requests.get(url, allow_redirects=True) #downloads the file
+    open('model.joblib', 'wb').write(r.content) #saves it as so
     pred = Predictor('SanitizedApplication.csv','model.joblib')
     features = ['reports','expenditure','active','income']
     df = pred.getSanitizedApplicationData()
     exc = pred.callExtractor(features)
     df = exc.FeatureExtraction(df)
     Model = pred.model_load()
-    out = pred.prediction_function(df,Model)
+    card = pred.prediction_function(df,Model)
+    id = rnd.sample_without_replacement(n_population=1000,n_samples=df.shape[0],method="reservoir_sampling")
+    df.insert(0,'ApplicationId',id)
+    df.insert(1,'Status',card)
+    df.to_csv('unapproved_prediction.csv', index=False)
