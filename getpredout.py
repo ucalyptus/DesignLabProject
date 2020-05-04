12abb725e3b912ef9dd1a40ad69a70dbe2885908
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-04 14:34:03 +0530

    Update getpredout.py

--- getpredout.py
+++ getpredout.py
@@ -6,6 +6,8 @@ import sys
 sys.path.append('../')
 
 ### data is a list of lists like data=[['tom', 10], ['nick', 15], ['juli', 14]]
+
+
 df = pd.DataFrame(data, columns = ['ApplicationId', 'Status']) 
 df.to_csv('prediction.csv')
 
