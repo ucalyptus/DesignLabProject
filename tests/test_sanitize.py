e06733743c1b0dd9bb2995ec84a55dab301bdd7a
muskan0508 <64703548+muskan0508@users.noreply.github.com>
2020-05-08 12:27:30 +0000

    Update test_sanitize.py

--- tests/test_sanitize.py
+++ tests/test_sanitize.py
@@ -10,12 +10,20 @@ import sanitize
 class TestSanitize(unittest.TestCase):
   
   def Test_getapplication(self):
-    
     obje = sanitize.Sanitizer('https://raw.githubusercontent.com/ucalyptus/Double-Branch-Dual-Attention-Mechanism-Network/master/UP.csv')
     df = obje.getApplication()
     assert_equal(df,pd.DataFrame)
-    
-    
+  def Columns(self, path_to_csv):
+    df = pd.read_csv(path_to_csv)
+    columns = list(df.columns)
+    try:
+      df_req = df[['reports', 'expenditure', 'active', 'income', 'ApplicationId']]
+    except e:
+      print("All necessary columns not found :")
+      print(e)
+    unique_ids = set(df['ApplicationId'])
+    assert len(df['ApplicationId']) == len(unique_ids) , "All IDs are not unique"
+    assert sum(df.isnull().sum()) == 0, "Null values found"
     
 
 
