8e24eebb2f3bac893922aa449e183e82d0ab59c1
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-08 12:58:21 +0530

    Create trainmodel.py

---
+++ trainmodel.py
@@ -0,0 +1,26 @@
+import pandas as pd
+#import ServerAPI as server_api
+import sys
+sys.path.append('../')
+
+class Trainer ():
+    def __init__(self,DownloadTrainingDataset) :
+        self.TrainingDataSet = DownloadTrainingDataset
+       
+    def callExtractor(ExtractData):
+        return Extractor(ExtractData)
+        
+    def train_gini_decision.fit(data,labels) :
+        // doubt
+    
+class Extractor():
+    def __init__(self,ExtractData):
+        self.ExtractData = ExtractData
+    
+    def FeatureExtraction(ExtractData):
+        return df[self.ExtractData]
+
+
+
+if __init__ == '__main__':
+    
