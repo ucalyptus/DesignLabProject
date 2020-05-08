021692be123b5d5e1114873b6d9e0aaaac4c1d95
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-08 14:38:19 +0530

    Update trainmodel.py

--- trainmodel.py
+++ trainmodel.py
@@ -1,28 +1,43 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import joblib
+from joblib import dump, load
+
 
 import sys
 sys.path.append('../')
 
 class Trainer ():
-    def __init__(self,DownloadTrainingDataset) :
-        self.TrainingDataSet = pd.read_csv(DownloadTrainingDataset)
+    def __init__(self) :
+        pass
        
-    def callExtractor(ExtractData):
-        return Extractor(ExtractData)
+    def callExtractor():
+        return Extractor(self.TrainingDataSet)
         
-    def train_gini_decision.fit(data,labels) :
-        // doubt
-    
+    def train_gini_decision_fit(X,y) :
+        X_train,_,y_train,_ = train_test_split(X,y,test_size=0.2,random_state=10)
+        classifier = DecisionTreeClassifier(criterion = "gini", random_state = 100,max_depth=5, min_samples_leaf=5)
+        classifier.fit(X_train,y_train)
+        return classifier 
+        
 class Extractor():
     def __init__(self,ExtractData):
         self.ExtractData = ExtractData
     
-    def FeatureExtraction(ExtractData):
-        return df[self.ExtractData]
+    def FeatureExtraction(df):
+        df.card.replace(['yes','no'], ['1', '0'], inplace=True)
+        X=df[["reports","expenditure","age","income"]]
+        y=df["card"]
+        return X,y
 
 
 
 if __init__ == '__main__':
-    tt  = Trainer('dataset.csv')
-    tt
+    df  = pd.read_csv('dataset.csv')
+    tt = Trainer()
+    exc = tt.callExtractor()
+    X,y = exc.FeatureExtraction(df)
+    clf = tt.train_gini_decision_fit(X,y)
+    dump(clf, 'model.joblib') 
+    
     
