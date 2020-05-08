b151c502f318457de5804f681ba13518d4818afa
muskan0508 <64703548+muskan0508@users.noreply.github.com>
2020-05-08 18:48:56 +0530

    Update test_getpredout.py

--- tests/test_getpredout.py
+++ tests/test_getpredout.py
@@ -10,13 +10,16 @@ class TestGetPredOut(unittest.TestCase):
   
   def Test_attribs(self):
     df = pd.read_csv('unapproved_prediction.csv')
-    assert('ApplicationId' in df.columns)
-    assert('Status' in df.columns)
+    assert('ApplicationId' in df.columns , "Invalid! Application Id mandatory ")
+    assert('Status' in df.columns , "Invalid! Status mandatory ")
     
   
   def Test_categorical(self):
     df = pd.read_csv('unapproved_prediction.csv')
-    assert('Yes' or 'No' in df.Status.values)
+    assert('Yes' or 'No' in df.Status.values , "Only yes/no allowed")
+    b = df.isnull().any().any()
+    self.assertFalse(b,"Null Values invalid ")
+
     
     
     
