506dddb0c348b6b740ce0f7f2b68cf04cd58dc63
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-05-08 14:56:25 +0530

    Update ServerAPI.py

--- ServerAPI.py
+++ ServerAPI.py
@@ -1,7 +1,15 @@
-import requests
+import ftplib
 
-
-
-
-def fetch_from(url):
+def download():
+    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
+    fil = open('application.csv','rb')
+    session.retrbinary('RETR application.csv',file)
+    fil.close()
+    session.quit()
+def upload():
     
+    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
+    fil = open('prediction.csv','rb')
+    session.storbinary('STOR prediction.csv',file)
+    fil.close()
+    session.quit()
