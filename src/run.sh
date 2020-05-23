a85f7201057cdf332a2bb6a8f4683cc0e0304448
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-23 20:08:14 +0000

    better code

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,4 @@
+wget https://gist.githubusercontent.com/ucalyptus/5ea2c461d2949e2fe3235fd9406a9479/raw/e377e68091c55ba3cee496b49337d56e6801a502/dataset.csv
 python3 trainmodel.py
 python3 sanitize.py
 python3 pred.py
