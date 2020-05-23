e0ff9168e80f1e3f2d520217d884e3a8b2555bea
Sayantan Das <santaclaus30011998@gmail.com>
2020-05-23 19:29:50 +0000

    should work now

--- tests/test_getpredout.py
+++ tests/test_getpredout.py
@@ -11,14 +11,14 @@ import os
 class TestGetPredOut(unittest.TestCase):
   
   def Test_attribs(self):
-    df = pd.read_csv('../unapproved_prediction.csv')
-    assert('ApplicationId' in df.columns , "Invalid! Application Id mandatory ")
-    assert('Status' in df.columns , "Invalid! Status mandatory ")
+    df = pd.read_csv('../src/unapproved_prediction.csv')
+    assert 'ApplicationId' in df.columns , "Invalid! Application Id mandatory "
+    assert 'Status' in df.columns , "Invalid! Status mandatory "
     
   
   def Test_categorical(self):
-    df = pd.read_csv('../unapproved_prediction.csv')
-    assert('Yes' or 'No' in df.Status.values , "Only yes/no allowed")
+    df = pd.read_csv('../src/unapproved_prediction.csv')
+    assert 'Yes' or 'No' in df.Status.values , "Only yes/no allowed" 
     b = df.isnull().any().any()
     self.assertFalse(b,"Null Values invalid ")
 
