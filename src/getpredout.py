ae91e7f2f14532b9e5b7c1524ea4fd343de694a9
ucalyptus <santaclaus30011998@gmail.com>
2020-05-24 21:01:40 +0530

    Mended prediction csv file

--- src/getpredout.py
+++ src/getpredout.py
@@ -1,24 +1,24 @@
-import ftplib
 import pandas as pd
 import sys
 sys.path.append('../')
 from src import ServerAPI
 
 def check(element):
-	if element == '1':
+	if element == 1:
 		return 'Yes'
 	else:
 		 return 'No'
 
 def setApprovalPrediction(df):
-  df['Status'] = df['Status'].apply(check)
   df = df.drop(feat_cols,axis=1)
+  df['Status'] = df['Status'].apply(check)
   df.to_csv('prediction.csv', index=False)
-  
-feat_cols = ['reports','expenditure','active','income']
-df = pd.read_csv('../src/unapproved_prediction.csv')
-setApprovalPrediction(df)
+  return df 
 
-ServerAPI.upload()
+if __name__ == "__main__":
+    feat_cols = ['reports','expenditure','active','income']
+    df = pd.read_csv('../src/unapproved_prediction.csv')
+    df = setApprovalPrediction(df)
+    ServerAPI.upload()
 
 
