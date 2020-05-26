006401c70832edfd54f22d1567c939af94fc5b4a
muskan0508 <64703548+muskan0508@users.noreply.github.com>
2020-05-27 01:22:20 +0530

    Update test_getpredout.py

--- tests/test_getpredout.py
+++ tests/test_getpredout.py
@@ -21,6 +21,9 @@ class TestGetPredOut(unittest.TestCase):
     assert 'Yes' or 'No' in df.Status.values , "Only yes/no allowed" 
     b = df.isnull().any().any()
     self.assertFalse(b,"Null Values invalid ")
+    
+  def test_checkExtension(self):
+    
 
 if __name__ == '__main__':
     unittest.main()
