261c274285541b03ea2af5c70e2eb73c5c971a79
ucalyptus <santaclaus30011998@gmail.com>
2020-05-30 00:44:17 +0530

    changes 2

--- src/ServerAPI.py
+++ src/ServerAPI.py
@@ -1,15 +1,18 @@
 import ftplib
+url = 'ftp.drivehq.com'
+username = 'MavenDev'
+password = 'Teammaven123'
 
 def download():
-    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
+    session = ftplib.FTP(url,username,password)
     handle = open('application.csv','wb')
     session.retrbinary('RETR application.csv',handle.write)
     handle.close()
     session.quit()
+
 def upload():
-    
-    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
-    fil = open('prediction.csv','rb')
-    session.storbinary('STOR prediction.csv',fil)
-    fil.close()
+    session = ftplib.FTP(url,username,password)
+    handle = open('prediction.csv','rb')
+    session.storbinary('STOR prediction.csv',handle)
+    handle.close()
     session.quit()
