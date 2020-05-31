9b9cc2d843d97d9855b19ba910c4ccb597460f0b
ucalyptus <santaclaus30011998@gmail.com>
2020-05-31 22:01:50 +0530

    changes 5

--- tests/test_pred.py
+++ tests/test_pred.py
@@ -1,12 +1,10 @@
 import pandas as pd
-import pandas.testing as pt
-import pytest
-import pandas as pd
 import unittest
-import sys
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 import sklearn
 import sys
-import requests
 sys.path.append('../')
 from src import pred
 import os
@@ -30,11 +28,6 @@ class TestPrediction(unittest.TestCase):
     self.assertEqual(type(features),type(df.columns))
     
   def test_modelclass(self):
-    """url = 'https://raw.githubusercontent.com/ucalyptus/scikit-on-gRPC/master/model.joblib'
-                r = requests.get(url, allow_redirects=True) #downloads the file
-                f = open('../tests/model.joblib', 'wb')
-                f.write(r.content) #saves it as so
-                f.close()"""
     ob = pred.Predictor('../src/SanitizedApplication.csv','../src/model.joblib')
     Model = ob.model_load()
     assert(type(Model) == sklearn.ensemble._forest.RandomForestClassifier)
@@ -51,37 +44,15 @@ class TestPrediction(unittest.TestCase):
         boolean=True
     else:
         boolean=False
-    #boolean = df.equals(dframe)
-    self.assertTrue(boolean, "Wrong Prediction")
     
-    #Boolean_Check
-    #import pandas as pd
-    #import numpy as np
-    #df1=pd.read_csv("unapproved_prediction.csv")
-    #df2=pd.read_csv("ExpectedPrediction.csv")
-    #col1=df1['Status'].tolist()
-    #cola=df1['ApplicationId'].tolist()
-    #col2=df2['Status'].tolist()
-    #colb=df1['ApplicationId'].tolist()
-    #col1=np.array(col1)
-    #cola=np.array(cola)
-    #col2=np.array(col2)
-    #colb=np.array(colb)
-    #r=np.array_equal(col1,col2)
-    #r1=np.array_equal(cola,colb)
-    #print(r)
-    #print(r1)
+    self.assertTrue(boolean, "Wrong Prediction")
     
     
   def test_salary(self):
     df = pd.read_csv('../src/SanitizedApplication.csv')
     boolean = (df['income'] >= 1.2).all()
     self.assertTrue(boolean, "Invalid Salary")
- 
-    
-    
-    
-    
+
 
 if __name__ == '__main__':
   unittest.main()
