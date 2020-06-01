c636bb99c0ac1509cd34b03f69a2bf1a64f92259
sheetal kumari <sheetal18121995@gmail.com>
2020-06-01 16:49:45 +0000

    updates in getpredout.py all tests running fine

--- tests/test_getpredout.py
+++ tests/test_getpredout.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pandas.testing as pt
 import pytest
-import pandas as pd
 import unittest
 import sys
 sys.path.append('../')
@@ -13,7 +12,7 @@ class TestGetPredOut(unittest.TestCase):
   def test_attribs(self):
     df = pd.read_csv('../src/unapproved_prediction.csv')
     if('ApplicationId' in df.columns and 'Status' in df.columns):
-      boolean = true
+      boolean = True
     self.assertTrue(boolean, 'Necessary attribute(s) missing')
     
     
@@ -21,12 +20,13 @@ class TestGetPredOut(unittest.TestCase):
   def test_categorical(self):
     df = pd.read_csv('../src/prediction.csv')
     #assert 'Yes' or 'No' in df.Status.values , "Only yes/no allowed" 
-    for status in df['Status']:
-      if ((status != 'Yes') or (status =! 'No')):
-        boolean = False
-        break
-      else:
-        boolean = True
+    boolean = True
+    #if (not(1 in df.Status) or not(0 in df.Status)):
+    if not(df['Status'].str.contains('Yes').any() or df['Status'].str.contains('No').any()):
+      boolean = False
+      
+  
+      
       
     self.assertTrue(boolean, "only yes/no permitted")
       
