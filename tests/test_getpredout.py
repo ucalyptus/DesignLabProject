179df989679fb6f47275c92f9884c1e8b5b1d15d
12016002011017 <46737912+12016002011017@users.noreply.github.com>
2020-06-02 20:47:07 +0530

    Update test_getpredout.py

--- tests/test_getpredout.py
+++ tests/test_getpredout.py
@@ -18,23 +18,13 @@ class TestGetPredOut(unittest.TestCase):
     
   
   def test_categorical(self):
-    df = pd.read_csv('../src/prediction.csv')
+    df = pd.read_csv('../tests/categorical_values.csv')
     assert 'Yes' or 'No' in df.Status.values , "Only yes/no allowed" 
-    if (not(1 in df.Status) or not(0 in df.Status)):
-    if (not(df['Status'].str.contains('Yes')).any() | not(df['Status'].str.contains('No')).any()):
-   # countyes = df[df['Status'].str.contains('Yes')]
-    #countno = df[df['Status'].str.contains('No')]
-   # length = len(df.index)
-    #c = countyes.count() + countno.count()
-    #if(c < length).any():
+    if (not(df['Status'].str.contains('Yes')).any() or not(df['Status'].str.contains('No')).any()):
       boolean = False
     else:
       boolean = True
     
-      
-  
-      
-      
     self.assertTrue(boolean, "only yes/no permitted")
 
   def test_checkNull(self):
