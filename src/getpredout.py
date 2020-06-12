c4d89fb453d54f55bd09d2a93669dc4f529242f3
Forkbabu <sayantandas30011998@gmail.com>
2020-06-13 02:32:05 +0530

    Removed TimeStamp

--- src/getpredout.py
+++ src/getpredout.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 sys.path.append('../')
 from src import ServerAPI
-from datetime import datetime
+
 
 def check(element):
 	if element == 1:
@@ -12,9 +12,7 @@ def check(element):
 
 def setApprovalPrediction(df):
   df['Status'] = df['Status'].apply(check)
-  now = datetime.now()
-  date_time = now.strftime("_%d-%m-%Y_%H:%M:%S")
-  df.to_csv('prediction{}.csv'.format(date_time), index=False)
+  df.to_csv('prediction.csv', index=False)
   return df 
 
 if __name__ == "__main__":
