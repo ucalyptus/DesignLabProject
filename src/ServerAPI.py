289adf513ca3aaedf7f1f70d1da90253cfd51dbb
Forkbabu <sayantandas30011998@gmail.com>
2020-07-21 13:40:18 +0530

    Update ServerAPI.py

--- src/ServerAPI.py
+++ src/ServerAPI.py
@@ -7,9 +7,13 @@
 
 """
 import ftplib
+from secrets import FTPID,FTPPWD
+
+username = FTPID
+password = FTPPWD
 
 def download():
-    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
+    session = ftplib.FTP('ftp.drivehq.com',username,password)
     handle = open('application.csv','wb')
     session.retrbinary('RETR application.csv',handle.write)
     handle.close()
@@ -17,7 +21,7 @@ def download():
     print('Downloaded Successfully')
 
 def upload():
-    session = ftplib.FTP('ftp.drivehq.com','MavenDev','Teammaven123')
+    session = ftplib.FTP('ftp.drivehq.com',username,password)
     fil = open('prediction.csv','rb')
     session.storbinary('STOR prediction.csv',fil)
     fil.close()
